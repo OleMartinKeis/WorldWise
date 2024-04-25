6c22b9deb4bd3c049dc3d5e0a49fb9a1f518fd62
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-25 19:36:54 +0200

    Link to app from Homepage

--- src/pages/Homepage/index.jsx
+++ src/pages/Homepage/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import PageNav from "../../components/PageNav";
 
 function Homepage() {
@@ -5,6 +6,7 @@ function Homepage() {
         <div>
             <PageNav />
             <h1> Worldwise </h1>
+            <Link to="/app">Go to the app</Link>
         </div>
     );
 }
