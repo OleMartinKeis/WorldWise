57798b2e4b8e578a69c8f6f1426b7ad5ef176b50
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-25 08:50:54 +0200

    Added routes to the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/ProductPage";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+
 function App() {
     return (
         <>
-            <div>WorldWise</div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="product" element={<Product />} />
+                    <Route path="pricing" element={<Pricing />} />
+                </Routes>
+            </BrowserRouter>
+            <div>Test</div>
         </>
     );
 }
