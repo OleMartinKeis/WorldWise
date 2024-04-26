0860bd0418f3c77306f3236350a2b913080c3a54
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-26 10:21:57 +0200

    Import Sidebar to AppLayout

--- src/pages/AppLayout/index.jsx
+++ src/pages/AppLayout/index.jsx
@@ -1,10 +1,9 @@
-import AppNav from "../../components/AppNav";
+import Sidebar from "../../components/Sidebar";
 
 export default function AppLayout() {
     return (
         <div>
-            <AppNav />
-            APP
+            <Sidebar />
         </div>
     );
 }
