6e7cd11a30b7d06d13c134bf2614da498aa0d765
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-26 10:53:48 +0200

    Update AppNav

--- src/components/AppNav/index.jsx
+++ src/components/AppNav/index.jsx
@@ -1,3 +1,16 @@
+import styles from "./index.module.css";
+import { NavLink } from "react-router-dom";
 export default function AppNav() {
-    return <nav>App Navigation</nav>;
+    return (
+        <nav className={styles.nav}>
+            <ul>
+                <li>
+                    <NavLink to="cities">Cities</NavLink>
+                </li>
+                <li>
+                    <NavLink to="countries">Countries</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 }
