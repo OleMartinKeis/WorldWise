fcab4a3f34cdabefba3e07400209b1fc39101403
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-27 09:25:13 +0200

    Added URL and saves results as res

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,16 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 
+const URL = "http://localhost:9000";
+
 function App() {
     const [cities, setCities] = useState({});
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(function () {
-        async function fetchCities( {})
+        async function fetchCities() {
+            const res = await fetch(`${URL}`);
+        }
     }, []);
 
     return (
