b4fd973deeabdc6a82d41b8511ae47c74bd1e697
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-29 10:08:41 +0200

    Update CityItem.jsx

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -15,7 +15,7 @@ export default function CityItem({ city }) {
             <span className={styles.emoji}>{emoji}</span>
             <h3 className={styles.name}>{cityName}</h3>
             <time className={styles.date}>{formatDate(date)}</time>
-            <buton className={styles.deleteBtn}>&times;</buton>
+            <button className={styles.deleteBtn}>&times;</button>
         </li>
     );
 }
