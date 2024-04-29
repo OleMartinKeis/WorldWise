d54a4d728490f9c7459c17dd938dc84ffdda2bb6
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-29 13:52:44 +0200

    Adds the Button primary component to Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 
 import styles from "./Form.module.css";
+import Button from "./Button";
 
 export function convertToEmoji(countryCode) {
     const codePoints = countryCode
@@ -51,7 +52,7 @@ function Form() {
             </div>
 
             <div className={styles.buttons}>
-                <button>Add</button>
+                <Button type="primary">Add</Button>
                 <button>&larr; Back</button>
             </div>
         </form>
