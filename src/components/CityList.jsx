05726b99cc0dd80cdfb9ff8decd10dd4b8159a1a
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-29 09:48:21 +0200

    Added a .mao to map out each city

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,7 +1,14 @@
+import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 
 export default function CityList({ cities, isLoading }) {
     if (isLoading) return <Spinner />;
-    return <ul className={styles.cityList}>City</ul>;
+    return (
+        <ul className={styles.cityList}>
+            {cities.map((city) => (
+                <CityItem city={city} key={city.id} />
+            ))}
+        </ul>
+    );
 }
