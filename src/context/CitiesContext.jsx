b2522487cec473cb57dfc42308619218087fb231
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-04-30 09:30:44 +0200

    Moved all state and effects from app to context

--- src/context/CitiesContext.jsx
+++ src/context/CitiesContext.jsx
@@ -1,5 +1,24 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const CitiesContext = createContext();
 
-function CitiesProvider({ children }) {}
+function CitiesProvider({ children }) {
+    const [cities, setCities] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(function () {
+        async function fetchCities() {
+            try {
+                setIsLoading(true);
+                const res = await fetch(`${BASE_URL}/cities`);
+                const data = await res.json();
+                setCities(data);
+            } catch {
+                alert("There was an error loading data");
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        fetchCities();
+    }, []);
+}
