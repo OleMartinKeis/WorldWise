ecd94199d498be45e30a893b14c713bdc0c25112
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-05-03 08:24:18 +0200

    Update City.jsx
    Removed old TEMP DATA and import getCity and currentCity from useCities context

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,5 +1,6 @@
 import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
+import { useCities } from "../context/CitiesContext";
 
 const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -11,18 +12,8 @@ const formatDate = (date) =>
 
 function City() {
     const { id } = useParams();
-    const [searchParams, setSearchParams] = useSearchParams();
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
-    // TEMP DATA
-    const currentCity = {
-        cityName: "Lisbon",
-        emoji: "🇵🇹",
-        date: "2027-10-31T15:59:59.138Z",
-        notes: "My favorite city so far!",
-    };
 
-    const { cityName, emoji, date, notes } = currentCity;
+    const { getCity, currentCity } = useCities();
 
     return (
         <h1>
