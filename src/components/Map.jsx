7a64f139b389966972f8d9aa7445c7c1d24918b1
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-05-06 08:41:18 +0200

    Changes map style and scroll behaviour

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,21 +1,28 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
 
 export default function Map() {
+    const [mapPosition, setMapPosition] = useState([0, 30]);
     const navigate = useNavigate();
     const [searchParams, setSearchParams] = useSearchParams();
     const lat = searchParams.get("lat");
     const lng = searchParams.get("lng");
-    const position = [51.505, -0.09];
+
     return (
         <div className={styles.mapContainer} onClick={() => navigate("form")}>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+            <MapContainer
+                className={styles.map}
+                center={mapPosition}
+                zoom={13}
+                scrollWheelZoom={true}
+            >
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker position={mapPosition}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
