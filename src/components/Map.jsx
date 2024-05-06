215e9cd6487bf6dd475cf17575668e795b90ca56
OleMartinKeis <100193534+OleMartinKeis@users.noreply.github.com>
2024-05-06 08:51:22 +0200

    Adds a marker to all the cities visited

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -24,11 +24,16 @@ export default function Map() {
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
+                {cities.map((city) => (
+                    <Marker
+                        key={city.id}
+                        position={[city.position.lat, city.position.lng]}
+                    >
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </div>
     );
